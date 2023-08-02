d64d16a01844e4c1bba3cacc6effee93fe6c4d73
kallurianusha <kallurianu13marolix@gmail.com>
2023-08-02 10:34:37 +0530

    third commit

---
+++ file2.py
@@ -0,0 +1,109 @@
+#mathematical operators:
+#a = "programming "
+#b = "language"
+#print(a+b)
+
+#x = "python loop "
+#print(x*4)
+
+#membership operators:
+#s = input("enter main string:")
+#substr = input("enter substr:")
+#if substr in s:
+#    print(substr,"is found in main string")
+#else:
+#    print(substr,"is not found in main string")
+
+
+#access through indexing:
+#string = 'fashion'
+#print(string[2])
+
+#string = 'fashion'
+#print(string[-3])
+
+
+#string = 'fashion'
+#print(string[1:3])
+
+
+# multipleline string:
+#quote = """ 
+#never give up
+#never let down 
+#"""
+#print (quote)
+
+#message = """
+#life teach u lessons 
+#run along with success
+#"""
+#print(message)
+
+# comparing two strings:
+#s1 = "learning python"
+#s2 = "python is easy"
+#if s1 == s2:
+#    print("statement is true")
+#else:
+#    print("statement is false")
+
+
+#s1 = "learning python"
+#s2 = "python is easy"
+#if s1 <= s2:
+#    print("statement is true")
+#else:
+#    print("statement is false")
+
+
+#s1 = "python"
+#s2 = "java"
+#if s1 > s2:
+#    print("python is easy")
+#else:
+#    print("python is not easy")
+
+
+#s1 = input("enter string1:")
+#s2 = input("enter string2:")
+#if s1 == s2:
+#    print("string is true")
+#else :
+#    print("string is not true")
+
+
+#space removing:
+#a = " never give up "
+#print(len(a))
+#stri_str = a.strip()
+#left_str = a.lstrip()
+#right_str = a.rstrip()
+#print(len(left_strip))
+
+#substrings in main strings:
+#x = "i will learn coding"
+#print(x.rfind("l"))
+#print(x.find("e",8,12))
+
+
+#a = "red roses are beautiful"
+#print(a.find("s"))
+#print(a.find("o",7,15))
+
+#indexing method:
+#s = input("main string:")
+#s2 = input("sub string:")
+#if s.index(s2):
+#    print(s2,"string is found")
+#else:
+#    print(s2,"string not found")
+
+
+#name = "ramya krishna"
+#a = name.rindex("i")
+#print(a)
+
+#x = "barbecque nation"
+#print(x.count("n",10,15))
+
